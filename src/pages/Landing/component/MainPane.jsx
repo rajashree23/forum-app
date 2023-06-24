940d4dffc5e643efddae4d5c27ad12d380e21843
Rajashree Parhi <rajashree.parhi@factset.com>
2023-06-24 22:29:18 +0530

    fix

--- src/pages/Landing/component/MainPane.jsx
+++ src/pages/Landing/component/MainPane.jsx
@@ -8,7 +8,7 @@ export const MainPane = () => {
     sortType === "Latest"
       ? [...posts.posts].sort(
           (p1, p2) =>
-            new Date(p1.createdAt).getTime() - new Date(p2.createdAt).getTime()
+            new Date(p2.createdAt).getTime() - new Date(p1.createdAt).getTime()
         )
       : sortType === "Most Upvoted"
       ? [...posts.posts].sort((p1, p2) => getVoteCount(p2) - getVoteCount(p1))
